50ed39c18c2350694ae226a5e6035da4a9c32a6e
David Wicker <davidwickerhf@gmail.com>
2021-02-15 21:52:29 +0100

    Updated Hype API click interface & Added parse method

--- banking/scripts/hype.py
+++ banking/scripts/hype.py
@@ -1,5 +1,7 @@
-import click, json
+import click, json, datetime, os, csv, logging
+
 from banking import Hype
+from banking.utils import utils
 
 
 @click.group()
@@ -33,12 +35,42 @@ def balance(username, password, birthday):
     except Exception as error:
         click.echo(str(error))
 
+
+@hype.command()
+@click.option('--username', prompt=True, required=True, type=click.STRING, help='Your HYPE email')
+@click.password_option('--password', help='Your HYPE password', confirmation_prompt=False)
+@click.option('--birthday', required=False, type=click.STRING, default=None, help='Your birthday: yyyy-mm-dd')
+def profile(username, password, birthday):
+    hype = Hype()
+    try:
+        hype.login(username, password, birthday)
+    except Exception as error:
+        click.echo(str(error))
+
+    code = click.prompt('OTP Code')
+    try:
+        hype.otp2fa(code)
+    except Exception as error:
+        click.echo(str(error))
+
+    try:
+        info = hype.get_profile_info()
+        click.echo('')
+        for key in info:
+            click.echo(f'{key}: {info[key]}')
+    except Exception as error:
+        click.echo(str(error))
+
+
 @hype.command()
 @click.option('--username', prompt=True, required=True, type=click.STRING, help='Your HYPE email')
 @click.password_option('--password', help='Your HYPE password', confirmation_prompt=False)
 @click.option('--birthday', required=False, type=click.STRING, default=None, help='Your birthday: yyyy-mm-dd')
 @click.option('--limit', required=False, default=999999, type=click.INT, help='Amount of movements to retrieve')
-def movements(username, password, birthday, limit):
+@click.option('--csvfile', is_flag=True, required=False, default=False, help='Use this flag to output the transactions in a CSV file')
+@click.option('--jsonfile', is_flag=True, required=False, default=False, help='Use this flag to output the transactions in a JSON file')
+@click.option('--output', type=click.Path(exists=True, dir_okay=True), help="The path of the output folder", required=False, default=None)
+def movements(username, password, birthday, limit, csvfile, jsonfile, output):
     hype = Hype()
     try:
         hype.login(username, password, birthday)
@@ -52,10 +84,70 @@ def movements(username, password, birthday, limit):
         click.echo(str(error))
 
     try:
+        # Get & Format movements
         movements = hype.get_movements(limit)
-        with open('movements.json', 'w+') as file:
-            json.dump(movements, file)
+        parsed = utils.parse_movements(movements)
+
+        # Output in console
+        click.echo('')
+        if not csvfile and not jsonfile:
+            for index, movement in enumerate(parsed):
+                for key in movement:
+                    click.echo(f'{key}: {movement[key]}')
+                click.echo()
+            return
+        
+        if not output:
+            while True:
+                output = click.prompt('Input the path of the desired output folder')
+                if os.path.isdir(output):
+                    break
+
+        # Get user code
+        profile = hype.get_profile_info()
+        user = str(profile.get('userCode'))
+
+        # Format timestamp
+        date = movements.get('end')
+        date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
+        date = int(date.timestamp())
+
+        # Output JSON file
+        if jsonfile:
+            filename = f'{output}/{date}_{user}.json' if output else f'{date}_{user}.json'
+            with open(filename, 'w+') as file:
+                json.dump(parsed, file)
+                click.echo(f'Movements saved to JSON file {filename}')
+
+        # Output CSV file
+        if csvfile:
+            filename = f'{output}/{date}_{user}.csv' if output else f'{date}_{user}.csv'
+            columns = ['id', 'title', 'amount', 'income', 'date', 'subType', 'category', 'reference', 'name', 'surname']
+
+            rows = list()
+            for movement in parsed:
+                data = list()
+                data.append(movement.get('id'))
+                data.append(movement.get('title'))
+                data.append(movement.get('amount'))
+                data.append(movement.get('income'))
+                data.append(movement.get('date'))
+                data.append(movement.get('subType'))
+                data.append(movement.get('category'))
+                data.append(movement.get('reference'))
+                data.append(movement.get('name'))
+                data.append(movement.get('surname'))
+                rows.append(data)
+
+            with open(filename, 'w+') as file:
+                writer = csv.writer(file)
+                writer.writerow(columns)
+                writer.writerows(rows)
+                click.echo(f'Movements saved to CSV file {filename}')
+
+
     except Exception as error:
+        logging.warning('Error', exc_info=error)
         click.echo(str(error))
 
 
--- banking/utils/utils.py
+++ banking/utils/utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from typing import Dict, List
 from lxml import html
 
 
@@ -26,3 +27,49 @@ def loginrequired(func):
             raise Exception("Login required but not yet performed")
         return func(self, *args, **kwargs)
     return wrapper
+
+
+def parse_movements(data:dict):
+    """Parse the json response of the Hype API movements endpoint
+
+    Args:
+        data (dict): Data returned by the Hype API
+
+    Returns:
+        List[Dict]: A list of the parsed movements.
+    """
+    if not data:
+        return None
+
+    movements:List[Dict] = list()
+
+    months = data.get('month')
+    if not months:
+        return None
+
+    for month in months:
+        movementsdata = month.get('movements')
+        if not movementsdata: pass
+        for movementdata in movementsdata:
+            transaction = dict()
+            transaction['id'] = movementdata.get('id')
+            transaction['title'] = movementdata.get('title')
+            transaction['date'] = movementdata.get('date')
+            
+            income = movementdata.get('income')
+            amount = movementdata.get('amount')
+            if not income: 
+                amount *= -1
+
+            transaction['income'] = income
+            transaction['amount'] = amount
+            transaction['subType'] = movementdata.get('subType')
+            additional = movementdata.get('additionalInfo')
+            transaction['category'] = additional.get('category').get('name') if additional else None
+            transaction['reference'] = additional.get('reference') if additional else None
+            transaction['name'] = additional.get('name') if additional else None
+            transaction['surname'] = additional.get('surName') if additional else None
+            movements.append(transaction)
+    return movements
+
+
